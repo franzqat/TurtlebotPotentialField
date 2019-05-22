6733d4fde92ee56f97d008e740ffc91a7a98ef1e
simone <simone.tro.94@gmail.com>
2019-05-22 20:31:00 +0200

    update controllo
    -Aggiunta classe ostacolo
    -Aggiunte funzioni per il calcolo delle forze attrattive e repulsive
    -Corretta forza repulsiva, ora usa la distanza dalla circonferenza come dovrebbe essere
    -Anche lo spostamento del robot è rappresentato con un cerchio per ogni punto di campionamento, il raggio del cerchio è basato sulla distanza_ruote/2+0.1 (il più 0.1 per dire che la ruota non è attaccata, esagerato forse, da discutere
    
    TODO:
    -Sistemare ulteriormente codice per aumentare ancora leggibilità
    -Aggiungere supporto ai driver

--- esempio.py
+++ esempio.py
@@ -112,7 +112,7 @@ class Robot:
 
 class PotentialFieldControl:
 
-    def __init__(self, robot, kp_lin, sat_lin, kp_angular, sat_angular, soglia, ostacoli, k_att, k_rep):
+    def __init__(self, robot, kp_lin, sat_lin, kp_angular, sat_angular, soglia, ostacoli, k_att):
         self.kp_lin = kp_lin
         self.sat_lin = sat_lin
         self.kp_angular = kp_angular
@@ -120,45 +120,36 @@ class PotentialFieldControl:
         self.robot = robot
         self.soglia=soglia
         self.ostacoli=ostacoli
-        self.k_att=k_att
-        self.k_rep=k_rep        
+        self.k_att=k_att       
 
     def evaluate(self, target_x, target_y):
 
         f_tot_x=0
         f_tot_y=0
         
-        for (x,y) in self.ostacoli:
-            d_x = x - self.robot.x
-            d_y = y - self.robot.y
-            d=math.hypot(d_x, d_y)
-            if(d<self.soglia):
-                f_rep=self.k_rep*((1/d**3)-(1/(d**2)*self.soglia))
-                angolo=math.atan2(d_y, d_x)
-                f_rep_x=f_rep*math.cos(angolo)
-                f_rep_y=f_rep*math.sin(angolo)
-                f_tot_x+=f_rep_x
-                f_tot_y+=f_rep_y
+        #forza repulsiva
+        for ostacolo in self.ostacoli:            
+            f_rep_x, f_rep_y=forza_repulsiva(ostacolo=ostacolo, 
+                                             posizione=(self.robot.x, self.robot.y),
+                                             soglia=self.soglia)           
+            f_tot_x+=f_rep_x
+            f_tot_y+=f_rep_y
             
-            
-        dx = target_x - self.robot.x
-        dy = target_y - self.robot.y
-
-        target_heading = math.atan2(dy, dx)
-        distance = math.hypot(dx, dy)
-
-        
-
-        f_att=self.k_att*distance
-        f_att_x=f_att*math.cos(target_heading)
-        f_att_y=f_att*math.sin(target_heading)
+        #forza attrattiva
+        f_att_x, f_att_y=forza_attrattiva(k_att=self.k_att, 
+                                          posizione=(self.robot.x, self.robot.y), 
+                                          target=(target_x, target_y))
         f_tot_x+=f_att_x
         f_tot_y+=f_att_y
         
-        f_tot=math.hypot(f_tot_x, f_tot_y)
-        ang=math.atan2(f_tot_y, f_tot_x)
         
+        f_tot=math.hypot(f_tot_x, f_tot_y)
+        
+        ang=math.atan2(f_tot_y, f_tot_x)        
         heading_error = ang - robot.theta
+        
+        
+        #controllo PSat
         v = self.kp_lin * f_tot
         w = self.kp_angular * heading_error
 
@@ -177,103 +168,101 @@ class PotentialFieldControl:
         return (vl, vr)
 
 
+class Ostacolo:
+    def __init__(self, centro, raggio, k_rep):
+        self.centro=centro
+        self.raggio=raggio
+        self.k_rep=k_rep
+
+def forza_attrattiva(k_att, posizione, target):
+    dx = target[0] - posizione[0]
+    dy = target[1] - posizione[1]
+    
+    #angolo verso l'obiettivo
+    target_heading = math.atan2(dy, dx)
+    
+    #distanza dall'obiettivo
+    distance = math.hypot(dx, dy)
+    
+    #forza attrattiva e componenti
+    f_att=k_att*distance
+    f_att_x=f_att*math.cos(target_heading)
+    f_att_y=f_att*math.sin(target_heading)
+    
+    return f_att_x, f_att_y
+
+def forza_repulsiva(ostacolo, posizione, soglia):
+    d_x = ostacolo.centro[0] - posizione[0]
+    d_y = ostacolo.centro[1] - posizione[1]
+    #la distanza minima è la distanza dlla circonferenza, cioè distanza_dal_centro-raggio
+    d=math.hypot(d_x, d_y)-ostacolo.raggio
+    
+    if d<soglia:
+        f_rep=ostacolo.k_rep*((1/d**3)-(1/(d**2)*soglia))
+        angolo=math.atan2(d_y, d_x)
+        f_rep_x=f_rep*math.cos(angolo)
+        f_rep_y=f_rep*math.sin(angolo)
+    else:
+        f_rep_x=0
+        f_rep_y=0
+        
+    return f_rep_x, f_rep_y
+
+
 
 delta_t = 0.001
-robot = Robot(6.0, 25.0, 0.3)
+
+distanza_ruote=0.3
+robot = Robot(6.0, 25.0, distanza_ruote)
 
 t = 0
 time_array = [ ]
-x_array = [ ]
-y_array = [ ]
+pos_array = [ ]
 theta = [ ]
 vl_array = [ ]
 vr_array = [ ]
 
-ostacoli=[(10,10),(25,20), (30,30),(40,40)]
-p = PotentialFieldControl(robot,1, 1.5,5, 4, 30, ostacoli, k_att=1, k_rep=10)
+ostacoli=[Ostacolo((10,10), 2, 20),
+          Ostacolo((10,30), 2, 20),
+          Ostacolo((15,20), 3, 20),
+          Ostacolo((20,10), 1, 20),          
+          Ostacolo((20,40), 1, 20),
+          Ostacolo((25,25), 1, 20),
+          Ostacolo((25,20), 2, 20),
+          Ostacolo((30,10), 2, 20),
+          Ostacolo((30,35), 3, 20),
+          Ostacolo((35,20), 1, 20),
+          Ostacolo((40,40), 2, 20),
+          Ostacolo((45,25), 4, 20)]
+          
+p = PotentialFieldControl(robot,1, 1.5,5, 4, 10, ostacoli, k_att=1)
 
 
 
-while t < 100:
+while t < 60:
 
     (vl, vr) = p.evaluate(50, 50)
 
     robot.evaluate(vl, vr, delta_t)
 
     time_array.append(t)
-    x_array.append(robot.x)
-    y_array.append(robot.y)
+    pos_array.append((robot.x, robot.y))
     vl_array.append(robot.current_vl)
     vr_array.append(robot.current_vr)
 
     t = t + delta_t
 
-print(robot.getPose())
 
-"""   
-while t < 30:
-
-    (vl, vr) = p.evaluate(0, 5)
-
-    robot.evaluate(vl, vr, delta_t)
-
-    time_array.append(t)
-    x_array.append(robot.x)
-    y_array.append(robot.y)
-    vl_array.append(robot.current_vl)
-    vr_array.append(robot.current_vr)
-
-    t = t + delta_t
-
-print(robot.getPose())    
-while t < 45:
-
-    (vl, vr) = p.evaluate(6, 6)
-
-    robot.evaluate(vl, vr, delta_t)
-
-    time_array.append(t)
-    x_array.append(robot.x)
-    y_array.append(robot.y)
-    vl_array.append(robot.current_vl)
-    vr_array.append(robot.current_vr)
-
-    t = t + delta_t
-
-print(robot.getPose()) 
-  
-while t < 60:
-
-    (vl, vr) = p.evaluate(0, 2)
-
-    robot.evaluate(vl, vr, delta_t)
-
-    time_array.append(t)
-    x_array.append(robot.x)
-    y_array.append(robot.y)
-    vl_array.append(robot.current_vl)
-    vr_array.append(robot.current_vr)
-
-    t = t + delta_t
-"""
-
-
-
-
-
+raggio_robot=distanza_ruote/2+0.1
 fig, ax = pylab.subplots()
-for (x,y) in ostacoli:
-    print(x, y)
-    cicle=plt.Circle((x,y), 2, color='b')
+for ostacolo in ostacoli:
+    cicle=plt.Circle(ostacolo.centro, ostacolo.raggio, color='b')
     plt.gcf().gca().add_artist(cicle)
 plt.axis([0, 60, 0, 60])
-plt.plot(x_array, y_array, 'r-+', label='posizione robot')
-plt.legend() 
+for (x,y) in pos_array:
+    cicle=plt.Circle((x,y), raggio_robot, color='r')
+    plt.gcf().gca().add_artist(cicle) 
 plt.show()
 
 
-#pylab.figure(2)
-#pylab.plot(time_array, vl_array, 'r-+', label='vl')
-#pylab.plot(time_array, vr_array, 'b-+', label='vr')
-#pylab.legend()
 
