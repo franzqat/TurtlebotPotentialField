9ec09d68696182cbe7375487e04795235d83e6c3
simone <simone.tro.94@gmail.com>
2019-06-21 18:02:36 +0200

    levati comandi per aggiungere ostacoli
    tanto non funzionavano
    
    -mi sembra buono il  codice non ha bisogno di molti commenti, visto vìche abbiamo usato nomi lunghi

--- MATERIALE-ESAME/test.py
+++ MATERIALE-ESAME/test.py
@@ -5,12 +5,16 @@ import sys
 from turtlebb import *
 from potentialfield_control import *
 
-
+#init ambiente statico
 k_repulsiva_ostacoli = 0.5
 ostacoli=[ Ostacolo((0.8,0.5), 0.1, k_repulsiva_ostacoli),
           Ostacolo((1.2,1), 0.1, k_repulsiva_ostacoli),
           Ostacolo((0.8,2), 0.1, k_repulsiva_ostacoli),
           Ostacolo((0.2,1.5), 0.1, k_repulsiva_ostacoli)]
+base=1.5 #m
+altezza=3 #m
+
+
 
 if __name__ == "__main__":
 
@@ -18,8 +22,8 @@ if __name__ == "__main__":
     t.open()
     t.setPose(105,105,0)
     d = PotentialFieldController(t, #bot
-                              1.5, ##base
-                              3,  #altezza
+                              base, 
+                              altezza,
                               0.01, #deltat
                               1,# kp_lin
                               0.22, #sat_lineare
@@ -44,9 +48,7 @@ if __name__ == "__main__":
             print("pose          	return the pose of the robot")
             print("pset x y th   	set the pose of the robot")
             print("speed l r     	set the speed of left and right wheels")
-            print("addo xc yc rg 	add obstacle")
             print("printo			print ostacoli")
-            print("removeo id		remove ostacolo id")
             print("stop            stop the robot")
 
         elif commands[0] == "pose":
@@ -72,19 +74,10 @@ if __name__ == "__main__":
         elif commands[0] == "go":
             setx=(int(commands[1]))
             sety=(int(commands[2]))
-            d.setTarget(float(setx)/1000, float(sety)/1000)
-###TODOTODOTODO check se gira qui ###
-        elif commands[0] == "addo":
-            ostacoli.append(Ostacolo((commands[1], commands[2]), commands[3], 0.5))
-            d.stop()
-            d.updateOstacoli(ostacoli)
-            d.start()			
+            d.setTarget(float(setx)/1000, float(sety)/1000)		
 
         elif commands[0] == "printo":
             print(ostacoli)
-
-        elif commands[0] == "removeo":
-            ostacoli.pop(commands[1])
 
         else:
             print("Invalid command")
